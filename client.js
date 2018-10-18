77d2fee10c14274bd7ccc88d7cd5586d163ce84f
Metamystical <metamystical@users.noreply.github.com>
2018-10-18 10:42:06 +0300

    remove dependency on bencode

--- client.js
+++ client.js
@@ -1,32 +1,33 @@
 const http = require('http')
-const ben = require('bencode')
+const encode = require('./encode-torrent')
+const decode = require('./decode-torrent')
 
 const dump = (data) => { console.log(data) }
 
 const ih = Buffer.from('3663c233ac0e1d329f538bab02128ba2c396467a', 'hex')
-// request(ben.encode({ method: 'announcePeer', args: { ih: ih } }), dump)
-// request(ben.encode({ method: 'getPeers', args: { ih: ih } }), dump)
+// request(encode({ method: 'announcePeer', args: { ih: ih } }), dump)
+// request(encode({ method: 'getPeers', args: { ih: ih } }), dump)
 
 const v = {m: 'JEB', f: 'MLK'}
-// request(ben.encode({ method: 'putData', args: { v: v, mutableSalt: false } }), dump)
+// request(encode({ method: 'putData', args: { v: v, mutableSalt: false } }), dump)
 /*
 request(
-  ben.encode({ method: 'makeImmutableTarget', args: { v: v } }),
-  (target) => { request(ben.encode({ method: 'getData', args: { target: target, mutableSalt: false } }), dump) }
+  encode({ method: 'makeImmutableTarget', args: { v: v } }),
+  (target) => { request(encode({ method: 'getData', args: { target: target, mutableSalt: false } }), dump) }
 )
 */
 const salt = ''
-// request(ben.encode({ method: 'putData', args: { v: v, mutableSalt: salt } }), dump)
+// request(encode({ method: 'putData', args: { v: v, mutableSalt: salt } }), dump)
 /*
 request(
-  ben.encode({ method: 'makeMutableTarget', args: { k: null, mutableSalt: salt } }), // k == null: use local publick key
-  (target) => { request(ben.encode({ method: 'getData', args: { target: target, mutableSalt: salt } }), dump) }
+  encode({ method: 'makeMutableTarget', args: { k: null, mutableSalt: salt } }), // k == null: use local publick key
+  (target) => { request(encode({ method: 'getData', args: { target: target, mutableSalt: salt } }), dump) }
 )
 */
 /*
 request(
-  ben.encode({ method: 'makeMutableTarget', args: { k: null, mutableSalt: salt } }),
-  (target) => { request(ben.encode({ method: 'putData', args: { v: v, mutableSalt: salt, resetTarget: target } }), dump) }
+  encode({ method: 'makeMutableTarget', args: { k: null, mutableSalt: salt } }),
+  (target) => { request(encode({ method: 'putData', args: { v: v, mutableSalt: salt, resetTarget: target } }), dump) }
 )
 */
 function request (post, done) {
@@ -43,7 +44,7 @@ function request (post, done) {
     (res) => {
       let data = Buffer.alloc(0)
       res.on('data', (chunk) => { data = Buffer.concat([data, chunk]) })
-      res.on('end', () => { try { data = ben.decode(data) } catch (err) { data = null }; done(data) })
+      res.on('end', () => { data = decode(data); done(data ? data.meta : data) })
     }
   ).end(post)
 }

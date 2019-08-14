069c8bacff54ad3182b477aebc39cd94731c5574
Metamystical <metamystical@users.noreply.github.com>
2019-08-14 21:13:04 +0300

    completed development

--- client.js
+++ client.js
@@ -1,50 +1,74 @@
+// client.js -- makes requests to server.js
+
+const DEFAULT_PORT = 6881
+
 const http = require('http')
-const encode = require('./encode')
-const decode = require('./decode')
 
-const dump = (data) => { console.log(data) }
+const client = {
+  serverPort: DEFAULT_PORT,
 
-const ih = Buffer.from('3663c233ac0e1d329f538bab02128ba2c396467a', 'hex')
-// request(encode({ method: 'announcePeer', args: { ih: ih } }), dump)
-// request(encode({ method: 'getPeers', args: { ih: ih } }), dump)
+  init: (port) => {
+    client.serverPort = port
+  },
 
-const v = {m: 'JEB', f: 'MLK'}
-// request(encode({ method: 'putData', args: { v: v, mutableSalt: false } }), dump)
-/*
-request(
-  encode({ method: 'makeImmutableTarget', args: { v: v } }),
-  (target) => { request(encode({ method: 'getData', args: { target: target, mutableSalt: false } }), dump) }
-)
-*/
-const salt = ''
-// request(encode({ method: 'putData', args: { v: v, mutableSalt: salt } }), dump)
-/*
-request(
-  encode({ method: 'makeMutableTarget', args: { k: null, mutableSalt: salt } }), // k == null: use local publick key
-  (target) => { request(encode({ method: 'getData', args: { target: target, mutableSalt: salt } }), dump) }
-)
-*/
-/*
-request(
-  encode({ method: 'makeMutableTarget', args: { k: null, mutableSalt: salt } }),
-  (target) => { request(encode({ method: 'putData', args: { v: v, mutableSalt: salt, resetTarget: target } }), dump) }
-)
-*/
-function request (post, done) {
+  announcePeer: (ih, next) => {
+    request({ method: 'announcePeer', args: { ih: ih } }, next)
+  },
+
+  getPeers: (ih, next) => {
+    request({ method: 'getPeers', args: { ih: ih } }, next)
+  },
+
+  putData: (v, mutableSalt, resetTarget, next) => {
+    request({ method: 'putData', args: { v: v, mutableSalt: mutableSalt, resetTarget: resetTarget } }, next)
+  },
+
+  getData: (target, mutableSalt, next) => {
+    request({ method: 'getData', args: { target: target, mutableSalt: mutableSalt } }, next)
+  },
+
+  makeMutableTarget: (k, mutableSalt) => {
+    request({ method: 'makeMutableTarget', args: { k: k, mutableSalt: mutableSalt } }, next)
+  },
+
+  makeImmutableTarget: (v, next) => {
+    request({ method: 'makeImmutableTarget', args: { v: v } }, next)
+  }
+}
+
+function request (data, done) {
+  const post = JSON.stringify(data)
   http.request(
     {
       hostname: 'localhost',
-      port: 6881,
+      port: client.serverPort,
       method: 'POST',
       headers: {
-        'Content-Type': 'application/octet-stream',
+        'Content-Type': 'application/json',
         'Content-Length': post.length
       }
     },
     (res) => {
       let data = Buffer.alloc(0)
       res.on('data', (chunk) => { data = Buffer.concat([data, chunk]) })
-      res.on('end', () => { done(decode(data)) })
+      res.on('end', () => {
+        data = JSON.parse(data)
+        walk(data)
+        done(data)
+      })
     }
-  ).end(post)
+  ).end(post).on('error', (err) => { done(null) })
 }
+
+function walk (obj) { // recursively walk through object, converting { type: 'Buffer', data: array of integers } to buffer
+  for (const k in obj) {
+    if (obj.hasOwnProperty(k)) {
+      const v = obj[k]
+      if (v && v.type === 'Buffer' && Array.isArray(v.data)) {
+        obj[k] = Buffer.from(v.data.map((i) => { let hex = i.toString(16); hex.length === 2 || (hex = '0' + hex); return hex }).join(''), 'hex')
+      } else if (!Array.isArray(obj) && typeof obj !== 'string' && !Buffer.isBuffer(obj)) walk(obj[k])
+    }
+  }
+}
+
+module.exports = client
